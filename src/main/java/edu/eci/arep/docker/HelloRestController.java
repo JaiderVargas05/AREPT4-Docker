b4ca366de2ac2d99a0c081c70c1e595ace3d6982
JVARGAS <jaider.vargas-n@mail.escuelaing.edu.co>
2025-09-14 19:44:48 -0500

    primera parte

---
+++ src/main/java/edu/eci/arep/docker/HelloRestController.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.eci.arep.docker;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author andrea.torres-g
+ */
+@RestController
+public class HelloRestController {
+
+private static final String template = "Hello, %s!";
+
+@GetMapping("/greeting")
+public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+return String.format(template, name);
+}
+}
